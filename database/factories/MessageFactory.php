75717062e766183c14bdbb22bb911aec94e5fe14
Paul Finch <finchy70@gmail.com>
2023-10-09 21:59:39 +0100

    Initial Laravel setup.

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Message;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MessageFactory extends Factory
+{
+    protected $model = Message::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
